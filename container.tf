06731d71d915f18dbfbf99605654d41481c486d4
Derek Hamel <msmtms1@gmail.com>
2025-12-26 20:39:11 -0600

    explicitly provide credentials for object storage

--- container.tf
+++ container.tf
@@ -16,7 +16,7 @@ terraform {
         skip_region_validation      = true
         skip_credentials_validation = true
         skip_metadata_api_check     = true
-        force_path_style            = true
+        use_path_style              = true
     }
 }
 
